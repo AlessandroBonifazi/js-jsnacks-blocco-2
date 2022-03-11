933fd41816d6367415280bf1418c29a8f4c401e1
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-11 15:24:48 +0100

    adding logic

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,16 @@
 // Debug
 console.log('JS OK!')
 
+// Crea un array di numeri interi. 
+// Fai la somma di tutti gli elementi che sono in posizione dispari.
+
+const array = [1, 3, 22, 47, 13, 21, 35];
+
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+    if (i % 2 == 0) {
+        sum += array[i];
+    }
+}
+console.log(sum);
