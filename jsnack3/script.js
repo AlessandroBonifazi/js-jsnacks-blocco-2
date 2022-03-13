a541de7e4656827ddac477e4334a11019569e804
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-13 19:17:39 +0100

    adding logic

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,22 @@
 // Debug
 console.log('JS OK!')
 
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro.
+
+let primoArray = ["james", "durant", "curry", "george", "leonard", "embiid", "morant"];
+
+let secondoArray = ["jordan", "magic", "bryant"];
+
+let element = "element";
+
+let i = 0;
+
+while (secondoArray.length < primoArray.length) {
+    secondoArray.push(element + i);
+    i++;
+}
+
+console.log(secondoArray.length);
+console.log(primoArray.length);
