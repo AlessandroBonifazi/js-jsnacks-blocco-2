06c138384081b9fbb21c4c5d937a267a39ad0eb9
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-14 10:46:34 +0100

    correzione

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,12 +4,12 @@ console.log('JS OK!')
 // Crea un array di numeri interi. 
 // Fai la somma di tutti gli elementi che sono in posizione dispari.
 
-const array = [1, 3, 22, 47, 13, 21, 35];
+const array = [1, 3, 22, 47, 13, 21];
 
 let sum = 0;
 
 for (let i = 0; i < array.length; i++) {
-    if (i % 2 == 0) {
+    if (i % 2 !== 0) {
         sum += array[i];
     }
 }
